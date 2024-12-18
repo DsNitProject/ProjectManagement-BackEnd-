eb5c8cb07fc6be3dcbf36a9e07fbeca5b5563044
mahdiyar <mahdiyarkhazaii@gmail.com>
2024-12-18 20:19:40 +0330

    creating the database orm again

--- BackEnd/database.py
+++ BackEnd/database.py
@@ -1,9 +1,9 @@
-import sqlalchemy
-import sqlalchemy as _sql
-import sqlalchemy.ext.declarative as _declarative
-import sqlalchemy.orm as _orm
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "sqlite:///database.db"
-engine = sqlalchemy.create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal=_orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base=_declarative.declarative_base()
+
+DATABASE_URL = "sqlite:///./database.db"
+engine = create_engine(DATABASE_URL,connect_args={"check_same_thread": False})
+sessionLocal= sessionmaker(autocommit=False,autoflush=False,bind=engine)
+Base = declarative_base()
