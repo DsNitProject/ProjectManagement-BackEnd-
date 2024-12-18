e8ced31928509c67da9897ebfc952f31133416dc
mahdiyar <mahdiyarkhazaii@gmail.com>
2024-12-18 22:55:32 +0330

    creating function to get db easier

--- BackEnd/database/database.py
+++ BackEnd/database/database.py
@@ -5,3 +5,10 @@ from sqlalchemy.ext.declarative import declarative_base
 engine = create_engine('sqlite:///database.db',connect_args={'check_same_thread': False})
 Base = declarative_base()
 Session = sessionmaker(bind=engine,autocommit=False,autoflush=False)
+
+def get_db():
+    db=Session()
+    try:
+        yield db
+    finally:
+        db.close()
