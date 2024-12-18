6b023be73a01fd7e495adb3e4ba8006be570a7d7
mahdiyar <mahdiyarkhazaii@gmail.com>
2024-12-18 22:23:14 +0330

    database update

---
+++ BackEnd/database/database.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:///database.db',connect_args={'check_same_thread': False})
+Base = declarative_base()
+Session = sessionmaker(bind=engine,autocommit=False,autoflush=False)
