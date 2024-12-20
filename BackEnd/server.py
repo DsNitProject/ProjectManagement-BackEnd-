54a5eb68cd5b78c4a1460c67f51e6d17649a91ee
mahdiyar <mahdiyarkhazaii@gmail.com>
2024-12-21 01:09:53 +0330

    creating the main file for backend

---
+++ BackEnd/server.py
@@ -0,0 +1,15 @@
+import uvicorn
+from fastapi import FastAPI
+from pip._internal.network import auth
+
+from BackEnd.routers import authuser
+from models import user
+from database.database import engine
+app = FastAPI()
+user.Base.metadata.create_all(bind=engine)
+app.include_router(authuser.router)
+@app.get("/")
+async def root():
+    return {"Hello": "World"}
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=7000)
