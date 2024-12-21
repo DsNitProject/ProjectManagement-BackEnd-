8caa3981b7b56f80acde342e13c8a5e574b2b5af
mahdiyar <mahdiyarkhazaii@gmail.com>
2024-12-21 04:45:07 +0330

    adding HashMap data structure for logging in and signin up

--- BackEnd/CRUD/user_crud.py
+++ BackEnd/CRUD/user_crud.py
@@ -1,15 +1,32 @@
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status
+
+from BackEnd.DataStructures.HashMap import HashMap
 from BackEnd.models import User, UserRole
 from BackEnd.schemas import UserCreate, UserUpdate
 from BackEnd.utils.hash import Hash
 
 
+user_cache=HashMap()
+
+
 def get_user(db: Session, user_id: int):
-    return db.query(User).filter(User.id == user_id).first()
+    cache_user=user_cache.users_byId(user_id)
+    if cache_user:
+        return cache_user
+    user=db.query(User).filter(User.id == user_id).first()
+    if user:
+        user_cache.add_user(user)
+    return user
 
 def get_user_by_email(db: Session, email: str):
-    return db.query(User).filter(User.email == email).first()
+    cache_user=user_cache.users_byEmail(email)
+    if cache_user:
+        return cache_user
+    user=db.query(User).filter(User.email == email).first()
+    if user:
+        user_cache.add_user(user)
+    return user
 
 
 def create_user(db: Session, user: UserCreate, role: UserRole = UserRole.user):
@@ -24,6 +41,7 @@ def create_user(db: Session, user: UserCreate, role: UserRole = UserRole.user):
         hashed_password=hashed_password,
         role=role
     )
+    user_cache.add_user(new_user) #اد کردن در هشمپ
     db.add(new_user)
     db.commit()
     db.refresh(new_user) #این برای برگرداندن ایدی ای که دیتابیس ساخته هستش
@@ -41,7 +59,7 @@ def update_user(db: Session, db_user: User, user_update: UserUpdate):
     if user_update.role and db_user.role != UserRole.admin:
         db_user.role = user_update.role
 
-
+    user_cache.add_user(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
